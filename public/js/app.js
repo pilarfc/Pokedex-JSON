c63defca039c6395d4f96ef11cd5144fa9a899a9
pilarfc <pilar.fc@outlook.com>
2017-06-12 17:37:11 +0000

    agregando modal a cada pokemon

--- public/js/app.js
+++ public/js/app.js
@@ -18,13 +18,13 @@ function agregarPokemones(pokemones){
     pokemones.forEach(function(pokemon, indice){ 
         var $li = $("<li />", {"data-indice": indice, "data-url": pokemon.url});
         $li.append($("<div />").css("background", "url(assets/img/pokemones_sprite.png)" + sprite + "px 0"));
-        $("#lista_pokemones").append($li.append($("<span />").text(pokemon.name)));
+        $("#lista_pokemones").append($li.append($(plantillaModal).text(pokemon.name)));
         sprite -= 80;
     });
     
 };
 
-var plantillaModal = '<a class="waves-effect waves-light btn" href="#modal">Características</a>';
+var plantillaModal = '<a class="waves-effect waves-light btn" href="#modal"></a>';
 
 // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
