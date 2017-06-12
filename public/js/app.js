588c691ab349dd1a3960614f40a074397277e22c
pilarfc <pilar.fc@outlook.com>
2017-06-12 21:46:08 +0000

    tratando de agregar detalles

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,15 @@
 var cargarPagina = function (){
   cargarPokemones();
-
+  cargarDetallesPokemones(); 
+   
 };
 
 var cargarPokemones = function () {
-  var url = "http://pokeapi.co/api/v2/pokemon/";
+    var url ='http://pokeapi.co/api/v2/pokemon-species/';
+//  var url = "http://pokeapi.co/api/v2/pokemon/";
 $.getJSON(url, function(response){
     var pokemones = response.results;
+    console.log(response);
     agregarPokemones(pokemones);
   });
 }; 
@@ -20,14 +23,15 @@ function agregarPokemones(pokemones){
         $li.append($("<div />").css("background", "url(assets/img/pokemones_sprite.png)" + sprite + "px 0"));
         $("#lista_pokemones").append($li.append($(plantillaModal).text(pokemon.name)));
         sprite -= 80;
-    });
-    
-};
+    });    
+}
 
 var plantillaModal = '<a class="waves-effect waves-light btn" href="#modal"></a>';
 
 // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+
+
 
 
 
